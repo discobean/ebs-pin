c6be55d3bf695f57772e82a393451a6a48098e5f
discobean <mariusz@stankiewicz.id.au>
2017-02-15 10:26:56 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from pip.req import parse_requirements
+from setuptools import setup
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='ebs-pin',
+    version='0.1',
+    scripts=['ebs-pin'],
+    packages=['stapler'],
+    install_requires=reqs
+)
+
