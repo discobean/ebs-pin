275551a5619ba1c7b635d72267a1307a5fb1bf83
discobean <mariusz@stankiewicz.id.au>
2017-02-15 14:51:05 +1100

    updated gitignore

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from pip.req import parse_requirements
 from setuptools import setup
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='ebs-pin',
-    version='0.1',
+    version='0.2',
     scripts=['scripts/ebs-pin'],
     packages=['ebspin'],
-    install_requires=reqs,
+    install_requires=[
+        'appdirs>=1.4.0',
+        'boto3>=1.4.4',
+        'botocore>=1.5.10',
+        'docutils>=0.13.1',
+        'futures>=3.0.5',
+        'jmespath>=0.9.1',
+        'packaging>=16.8',
+        'pyparsing>=2.1.10',
+        'python-dateutil>=2.6.0',
+        'requests>=2.13.0',
+        's3transfer>=0.1.10',
+        'six>=1.10.0'],
     keywords='ebs ebspin ebs-pin'
 )
 
